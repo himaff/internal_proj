a99ff17777923479f3d427aa3085e6d7a2b8b762
marclastar <marc_affechi@hotmail.com>
2016-07-08 18:40:25 +0000

    new projet entrepot compta...

---
+++ ap_czmgt/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'AP CZ management',
+    'version': '1.0.0',
+    'category': 'Liste',
+    'sequence': 3,
+    'author': 'Africa performances',
+    'summary': 'Manage members of website PAC',
+    'description': """
+Manage several members
+======================================
+
+This App for manage members of africa performances website PAC
+    """,
+    'depends': ["base"],
+    'data': [
+        'ap_pac.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
