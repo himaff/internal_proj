d6989a272aa9837ae34b7f52a4e45f3d72468d66
ASSALEF <assalefidele@yahoo.fr>
2016-07-11 13:34:02 +0000

    comit monday 11/07/16
    comit

---
+++ ap_om/ap_om.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import openerp
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+import time
+
+class ap_om(osv.osv):
+    _name = 'ap.om'
+    _order = 'ref asc'
+
+    _columns = {
+        'name_id': fields.many2one('hr.employee', 'name of employee'),
+        'ref': fields.char('Reference', size=128, required=True),
+        'project_name': fields.many2one('project.project', 'project name'),
+        'object': fields.char('object', size=128, required=True),
+        'route_to_go': fields.char('route to go', size=128, required=True),
+        'route_return': fields.char('route of return', size=128, required=True),
+        'date_hour_depart': fields.date('date hour depart', required=True),
+        'date_hour_return': fields.date('date hour return', required=True),
+        'accompanying_person': fields.many2many('hr.employee', 'fleet_vehicle_vehicle_tag_rel', 'vehicle_tag_id','tag_id', 'accompanying person', copy=False),
+        'means_of_transport': fields.many2one('fleet.vehicle', 'means_of_transport'),
+        'notes_de_frais': fields.many2one('hr.expense.expense', 'notes de frais', required=True),		
+        'active': fields.boolean('Active', help="If a om is not active, it will not be displayed in om"),
+    }
+
+    _defaults = {
+        'active' : True
+    }
+
+    _sql_constraints = [
+        ('uniq_ref', 'unique(ref)', "A om already exists with this ref in database. ref must be unique!"),
+        ('uniq_project_name', 'unique(project_name)', "A project_name already exists with this name in database. project_name must be unique!"),
+    ]
