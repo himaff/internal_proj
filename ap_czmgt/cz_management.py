108da75139b8d405f5304be83afc643580829ea5
Richmond FIKO <richmondfiko@africaperformances-ci.com>
2016-07-11 18:24:17 +0000

    commit ap_czmgnt 11072016

--- ap_czmgt/cz_management.py
+++ ap_czmgt/cz_management.py
@@ -26,16 +26,44 @@ class account_journal(osv.osv):
     _name = "account.journal"
     _inherit = "account.journal"
 
+    def on_change_user(self, cr, uid, ids, user_id, context=None):
+        """ When changing the user, also set a section_id or restrict section id
+            to the ones user_id is member of. """
+        section_id = self._get_default_section_id(cr, uid, user_id=user_id, context=context) or False
+        if user_id and self.pool['res.users'].has_group(cr, uid, 'base.group_multi_salesteams') and not section_id:
+            section_ids = self.pool.get('crm.case.section').search(cr, uid, ['|', ('user_id', '=', user_id), ('member_ids', '=', user_id)], context=context)
+            if section_ids:
+                section_id = section_ids[0]
+        return {'value': {'section_id': section_id}}
+
+    def _get_default_section_id(self, cr, uid, user_id=False, context=None):
+        """ Gives default section by checking if present in the context """
+        section_id = self._resolve_section_id_from_context(cr, uid, context=context) or False
+        if not section_id:
+            section_id = self.pool.get('res.users').browse(cr, uid, user_id or uid, context).default_section_id.id or False
+        return section_id
+	
+    def _resolve_section_id_from_context(self, cr, uid, context=None):
+        """ Returns ID of section based on the value of 'section_id'
+            context key, or None if it cannot be resolved to a single
+            Sales Team.
+        """
+        if context is None:
+            context = {}
+        if type(context.get('default_section_id')) in (int, long):
+            return context.get('default_section_id')
+        if isinstance(context.get('default_section_id'), basestring):
+            section_ids = self.pool.get('crm.case.section').name_search(cr, uid, name=context['default_section_id'], context=context)
+            if len(section_ids) == 1:
+                return int(section_ids[0][0])
+        return None
+
     _columns = {
         'section_id': fields.many2one('crm.case.section','Team', help="CZ Team")
     }
-	
-    def on_change_journal_section_id(self, cr, uid, ids, user_id, context=None):
-        values = {}
-        if user_id:
-            user = self.pool.get('res_users').browse(cr, uid, user_id, context=context)
-            values = {'section_id': user.default_section_id,}
-        return {'value': values}
+    _defaults = {
+        'section_id': lambda s, cr, uid, c: s._get_default_section_id(cr, uid, context=c),
+    }
 
 class account_voucher(osv.osv):
     _name = "account.voucher"
