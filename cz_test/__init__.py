59ca7fd02f60d6cd9094346001b093d7d1f98a89
Richmond FIKO <richmondfiko@africaperformances-ci.com>
2016-07-12 14:57:15 +0000

    ok cleaning

--- cz_test/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# © <2016> <Africa Performances>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-import cz_management
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
