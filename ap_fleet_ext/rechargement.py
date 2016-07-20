992a406a27f0675e026053120c6c920d8d232b4f
ASSALEF <assalefidele@yahoo.fr>
2016-07-20 16:00:47 +0000

    commit ap_fleet_ext 20/07/2016 16h01

--- ap_fleet_ext/rechargement.py
+++ ap_fleet_ext/rechargement.py
@@ -13,10 +13,13 @@ class rechargement(osv.osv):
 		'card_number': fields.char('Numero de la carte', size=128, required=True),
 		'vehicle': fields.char('Vehicule', size=128, required=True),
 		'user': fields.char('Utilisateur', size=128, required=True),
-		'amount_before_reloading': fields.char('Somme avant rechargement', size=128, required=True),
-		'reloading': fields.char('Rechargement', size=128, required=True),
-		'bonus_fcfa': fields.char('Bonus en FCFA', size=128, required=True),
-		'amount_after_reloading': fields.char('Somme apres rechargement', size=128, required=True),
+		'amount_before_reloading': fields.float('Solde avant rechargement', digits=(2,1)),
+		'reloading': fields.float('Rechargement', digits=(2,1)),
+		'bonus_fcfa': fields.float('Bonus en fcfa', digits=(2,1)),
+		'amount_after_reloading': fields.float('Solde apres rechargement', digits=(2,1)),
+		'amount_reload_total': fields.float('Montant total des rechargement', digits=(2,1)),
+		'total_bonus_fcfa': fields.float('Bonus total en fcfa', digits=(2,1)),
+		'total_balance_after_reload': fields.float('Solde total apres rechargement', digits=(2,1)),
     }
 
     _defaults = {
