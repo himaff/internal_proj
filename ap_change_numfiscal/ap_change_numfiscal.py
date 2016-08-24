59b1eefb1b9d65e053d946c0fe284bc85c864191
ASSALEF <assalefidele@yahoo.fr>
2016-08-24 12:58:36 +0000

    commit ap_change_numfiscal le 24/08/16   à 13h00

--- ap_change_numfiscal/ap_change_numfiscal.py
+++ ap_change_numfiscal/ap_change_numfiscal.py
@@ -7,32 +7,16 @@ from openerp.tools.translate import _
 from datetime import datetime
 
     
-
-class ap_change_posfiscal(osv.osv):
+class ap_change_numfiscal(osv.osv):
     _name='res.partner'
     _inherit ='res.partner'
 
     _columns = {
-        'reverse_charge_vat': fields.many2one('account.fiscal.position', 'Reverse charge V.A.T N°'),
-    }
-
-
-class ap_change_numfiscal(osv.osv):
-    _name='account.invoice'
-    _inherit ='account.invoice'
-
-    _columns = {
-        'reverse_charge_vat': fields.many2one('account.fiscal.position', 'Reverse charge V.A.T N°'),
+        'vat': fields.char('Reverse charge V.A.T N°', help="Tax Identification Number. Check the box if this contact is subjected to taxes. Used by the some of the legal statements."),
     }
 	
 	
-class ap_suprim_numfiscal(osv.osv):
-    _name='account.invoice'
-    _inherit ='account.invoice'
 	
-    def _compute_payments(self):
-        fiscal_position = fields.many2one('account.fiscal.position', string='Fiscal Position',
-        readonly=True, states={'draft': [('readonly', False)]})
 	
 
 
