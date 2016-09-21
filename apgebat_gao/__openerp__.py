ab893df89bcdebae62e3b2f62ecf1cd497313e76
marclastar <marc_affechi@hotmail.com>
2016-09-21 22:13:22 +0000

    v 1.2

--- apgebat_gao/__openerp__.py
+++ apgebat_gao/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'AP Gestion des appels d\'offres',
-    'version': '1.0.0',
+    'version': '1.0.2',
     'category': 'Liste',
     'sequence': 3,
     'author': 'Africa performances',
